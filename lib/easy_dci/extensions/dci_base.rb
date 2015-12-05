3493c7943b3877ea6053d378112fdf98d27ba8f4
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2015-12-05 08:00:52 +0100

    Handle additional params for DCI actions

--- lib/easy_dci/extensions/dci_base.rb
+++ lib/easy_dci/extensions/dci_base.rb
@@ -11,18 +11,22 @@ module EasyDCI
 
       def create
         set_dci_data(_crud_model.params(:on_create))
-        _crud_model.scoped? ? call_dci_context(:create, _crud_scoped_object) : call_dci_context(:create)
+        if _crud_model.scoped? || _crud_model.polymorphic?
+          call_dci_context(:create, _crud_scoped_object, *_dci_additional_params_on_create)
+        else
+          call_dci_context(:create, *_dci_additional_params_on_create)
+        end
       end
 
 
       def update
         set_dci_data(_crud_model.params(:on_update))
-        call_dci_context(:update, _crud_object)
+        call_dci_context(:update, _crud_object, *_dci_additional_params_on_update)
       end
 
 
       def destroy
-        call_dci_context(:delete, _crud_object)
+        call_dci_context(:delete, _crud_object, *_dci_additional_params_on_destroy)
       end
 
 
@@ -32,6 +36,21 @@ module EasyDCI
         def do_render_flash_messages?
           return self.render_flash_messages unless self.render_flash_messages.is_a?(Proc)
           self.render_flash_messages.call(request)
+        end
+
+
+        def _dci_additional_params_on_create
+          []
+        end
+
+
+        def _dci_additional_params_on_update
+          []
+        end
+
+
+        def _dci_additional_params_on_destroy
+          []
         end
 
     end
