f9a1d95939d5621d2f6ae8b92d54171b4b2548d8
Nicolas Rodriguez <nrodriguez@jbox-web.com>
2015-12-12 09:12:03 +0100

    Handle nested_form

--- app/helpers/easy_dci/application_helper.rb
+++ app/helpers/easy_dci/application_helper.rb
@@ -2,9 +2,14 @@ module EasyDCI
   module ApplicationHelper
 
     def dci_form_for(object, opts = {}, &block)
+      nested = opts.delete(:nested) { false }
       layout = request.xhr? ? :default : :horizontal
       opts   = opts.reverse_merge(remote: request.xhr?, layout: layout)
-      bootstrap_form_for(object, opts, &block)
+      if nested
+        bootstrap_nested_form_for(object, opts, &block)
+      else
+        bootstrap_form_for(object, opts, &block)
+      end
     end
 
   end
